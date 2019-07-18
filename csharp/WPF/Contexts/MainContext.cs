495f4b9419939995ccdf63db56cd88521a9265f6
NorArax NeflinOwl <pudwel.n.patryk@gmail.com>
2019-07-18 17:19:31 +0200

    Add new WPF project in csharp folder

---
+++ csharp/WPF/Contexts/MainContext.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+using System.Windows;
+
+namespace WPF.Contexts
+{
+    public class MainContext
+    {
+        private static object locker = new object();
+        private static MainContext instance = new MainContext();
+
+        public static MainContext Instance { get { lock (locker) return instance; } }
+
+        public IList<Window> Windows { get; private set; }
+
+        private MainContext()
+        {
+            Windows = new List<Window>();
+        }
+    }
+}
